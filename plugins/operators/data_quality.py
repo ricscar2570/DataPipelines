36813257f4ad619dc67f3a203d7ebd3fb6baffe1
riccardo <scaringi_riccardo@libero.it>
2025-02-12 16:20:19 +0100

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -3,20 +3,22 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class DataQualityOperator(BaseOperator):
-
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id='',
+                 tables=[],
                  *args, **kwargs):
-
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        for table in self.tables:
+            self.log.info(f'Checking data quality for table {table}')
+            records = redshift.get_records(f'SELECT COUNT(*) FROM {table}')
+            if not records or not records[0][0]:
+                raise ValueError(f'Data quality check failed for {table}: No records found')
+            self.log.info(f'Data quality check passed for {table}')
